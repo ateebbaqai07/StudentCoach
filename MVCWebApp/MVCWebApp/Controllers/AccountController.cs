ba7673cc104b5e6810f1a52ba4c2c8bf2c37d698
ateebbaqai07 <30473387+ateebbaqai07@users.noreply.github.com>
2017-07-26 10:46:55 -0700

    dsfsdf

--- MVCWebApp/MVCWebApp/Controllers/AccountController.cs
+++ MVCWebApp/MVCWebApp/Controllers/AccountController.cs
@@ -12,7 +12,7 @@ using MVCWebApp.Models;
 
 namespace MVCWebApp.Controllers
 {
-    [Authorize] 
+    [Authorize]      
     public class AccountController : Controller
     {
         private ApplicationSignInManager _signInManager;
